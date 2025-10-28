00bf3f12011276c970c0e4d50d1862d66351763a
TARKHO <mohmadapp2006@gmail.com>
2025-10-28 23:02:54 +0300

    Финальные исправления и форматирование кода

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ total = 0
 for i in range(1, 11):
     total += i
 print("Сумма чисел от 1 до 10 равна:", total)
+
 average = total / 10
 print("Среднее значение:", average)
+
 print("Давлетмурзаев М-Э")
 print("ИВТ-б-о-24-1")
